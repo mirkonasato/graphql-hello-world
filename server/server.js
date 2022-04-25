3c5fa1412e038ac1c6717cd278df95239551ea8e
Mirko Nasato <mirko@nasato.dev>
2022-04-25 10:40:09 +0100

    Resolver Functions

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
-import { gql } from 'apollo-server';
+import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`
   type Query {
     greeting: String
   }
 `;
+
+const resolvers = {
+  Query: {
+    greeting: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await server.listen({ port: 9000 });
+console.log(`Server running at ${url}`);
